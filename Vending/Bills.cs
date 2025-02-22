7f2aca4c8fd3e3817294a078411528187432af15
InventEverything <ian.engle@student.centralia.edu>
2025-02-21 19:46:13 -0800

    Added gitignore and missing classes.
    Not sure why Bills and MoneyHandler were not ingluded in previous
    commit. Added a gitignore for .vs folder.

---
+++ Vending/Bills.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending
+{
+    public class Bills
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public Bills(int id, string name, int value)
+        {
+            Id = id;
+            Name = name;
+            Value = value;
+        }
+    }
+}
---
+++ Vending/MoneyHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending
+{
+    public class MoneyHandler
+    {
+        private List<Bills> _bills;
+
+        public MoneyHandler()
+        {
+            _bills = new List<Bills>
+            {
+                new Bills(1, "$1", 1),
+                new Bills(2, "$5", 5),
+                new Bills(3, "$10", 10),
+                new Bills(4, "$20", 20)
+            };
+        }
+        public string Id(int id)
+        {
+            foreach (var b in _bills)
+            {
+                if (b.Id == id) { return b.Id.ToString(); }
+            }
+            return null;
+        }
+        public string Bill(int id)
+        {
+            foreach (var b in _bills)
+            {
+                if (b.Id == id) { return b.Name; }
+            }
+            return null;
+        }
+        public int Value(int id)
+        {
+            foreach (var b in _bills)
+            {
+                if (b.Id == id) { return b.Value; }
+            }
+            return 0;
+        }
+    }
+}
